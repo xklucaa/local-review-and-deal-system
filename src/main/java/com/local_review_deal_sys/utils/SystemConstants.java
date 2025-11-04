c56a08ab9319805f74706f5c67d35fd2297abec1
jerrymis <1831433004@qq.com>
2025-11-04 18:02:41 +0800

    modified

--- src/main/java/com/local_review_deal_sys/utils/SystemConstants.java
+++ src/main/java/com/local_review_deal_sys/utils/SystemConstants.java
@@ -1,7 +1,7 @@
 package com.local_review_deal_sys.utils;
 
 public class SystemConstants {
-    public static final String IMAGE_UPLOAD_DIR = "/usr/share/nginx/html/src/components/imgs";
+    public static final String IMAGE_UPLOAD_DIR = "/usr/share/nginx/html/imgs";
     public static final String USER_NICK_NAME_PREFIX = "user_";
     public static final int DEFAULT_PAGE_SIZE = 5;
     public static final int MAX_PAGE_SIZE = 10;
