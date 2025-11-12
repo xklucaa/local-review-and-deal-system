b15ffb25311c4e19fea0d439dcd3131a0ef8ef44
XIONG KUN <167501387+xklucaa@users.noreply.github.com>
2025-11-12 11:58:13 +0800

    Update SeckillVoucherServiceImpl.java

--- src/main/java/com/local_review_deal_sys/service/impl/SeckillVoucherServiceImpl.java
+++ src/main/java/com/local_review_deal_sys/service/impl/SeckillVoucherServiceImpl.java
@@ -6,15 +6,9 @@ import com.local_review_deal_sys.service.ISeckillVoucherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 秒杀优惠券表，与优惠券是一对一关系 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2022-01-04
- */
+
 @Service
 public class SeckillVoucherServiceImpl extends ServiceImpl<SeckillVoucherMapper, SeckillVoucher> implements ISeckillVoucherService {
 
 }
+
