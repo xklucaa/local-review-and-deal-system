f7145f9ca7faa019028a00cda842b49e1f6d5962
luca <xiongkun778@163.com>
2025-11-12 12:05:31 +0800

    refactor

--- src/main/java/com/local_review_deal_sys/dto/PasswordLoginForm.java
+++ src/main/java/com/local_review_deal_sys/dto/PasswordLoginForm.java
@@ -3,11 +3,7 @@ package com.local_review_deal_sys.dto;
 import lombok.Data;
 
 /**
- * @author admin
- * @version 1.0.0
- * @ClassName PasswordLoginForm.java
- * @Description TODO
- * @createTime 2025年10月14日 10:42:00
+
  */
 @Data
 public class PasswordLoginForm {
